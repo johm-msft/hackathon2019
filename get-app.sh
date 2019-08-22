422adfaefa34e56b5b2c3a5eed1cdd45148ab3df
johm-msft <johm@microsoft.com>
2019-08-22 15:57:06 -0700

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -3,7 +3,7 @@ size=""
 ulimit -n 20000
 while :
 do
-	/usr/bin/wget -O /opt/md5 https://raw.githubusercontent.com/catengineering/hackathon2019/md5
+	/usr/bin/wget -O /opt/md5 https://raw.githubusercontent.com/catengineering/hackathon2019/master/md5
 	/usr/bin/wget -O /opt/server https://github.com/catengineering/hackathon2019/raw/master/server
 	sleep 5
 	md5=$(cat /opt/md5)
